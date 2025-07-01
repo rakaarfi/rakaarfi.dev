80e7c07df3133cb3270fe687a38b30db9fd12453
rakaarfi <rakaarfi@gmail.com>
2025-07-01 19:56:32 +0800

    add log for debugging tawk id (third trial)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
+console.log('BUILD: NEXT_PUBLIC_TAWK_TO_ID =', process.env.NEXT_PUBLIC_TAWK_TO_ID);
+
 const nextConfig = {
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  env: {
+    NEXT_PUBLIC_TAWK_TO_ID: process.env.NEXT_PUBLIC_TAWK_TO_ID,
+  },
 };
 
 module.exports = nextConfig;
