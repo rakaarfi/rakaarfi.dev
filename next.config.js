ae8bba0cfa0a79af315a7a8d7746d28b7bcebf2f
rakaarfi <rakaarfi@gmail.com>
2025-07-01 20:31:15 +0800

    debug env var

--- next.config.js
+++ next.config.js
@@ -2,14 +2,10 @@
 console.log('BUILD: NEXT_PUBLIC_TAWK_TO_ID =', process.env.NEXT_PUBLIC_TAWK_TO_ID);
 
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  env: {
-    NEXT_PUBLIC_TAWK_TO_ID: process.env.NEXT_PUBLIC_TAWK_TO_ID,
-  },
 };
 
 module.exports = nextConfig;
